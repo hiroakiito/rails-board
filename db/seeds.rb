815e3d6cedac395ce8e3ce20faf4d5ea0eee6636
hiroakiito <pigaru.gooood.mar19@gmail.com>
2019-01-24 18:11:14 +0900

    77

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,13 @@ if Rails.env == 'development'
   (1..50).each do |i|
     Board.create(name: "ユーザー#{i}", title: "タイトル#{i}", body: "本文#{i}")
   end
+
+  Tag.create([
+    { name: '青森' },
+    { name: '秋田' },
+    { name: '岩手' },
+    { name: '宮城' },
+    { name: '山形' },
+    { name: '福島' },
+    ])
 end
