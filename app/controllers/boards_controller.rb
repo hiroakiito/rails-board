f4da489c8cbf2bd89cf28313f17db2b16dd36d2e
hiroakiito <pigaru.gooood.mar19@gmail.com>
2019-01-24 15:35:13 +0900

    最初のpush

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,43 @@
+class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+  def index
+    @boards = Board.all.page(params[:page])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    board = Board.create(board_params)
+    redirect_to board#boardのIDのページに飛んでくれる
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @board.update(board_params)
+    redirect_to board#(オブジェクトの詳細画面へ繊維)
+  end
+
+  def destroy
+
+    @board.delete
+
+    redirect_to boards_path
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module App
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    I18n.config.available_locales = :ja
+    I18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+  end
+end
